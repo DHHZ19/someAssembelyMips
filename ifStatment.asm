0b9387fd807726c04999d83cee1abf76136d32fb
Diego Hernandez <62413339+DHHZ19@users.noreply.github.com>
2022-10-25 08:17:33 -0500

    Update ifStatment.asm

--- ifStatment.asm
+++ ifStatment.asm
@@ -5,8 +5,8 @@ b += a;
 }
 
   slt $t0, $s0, $s1 #asks a > b
-  bne $t0, $zero, else: # sets the value
-  add $s1, $s1, $s0 #if true does this
+  bne $t0, $zero, else: # asks if $t0, != $zero if true then branch of to else: label
+  add $s1, $s1, $s0 # this is the statment called if they are equal
   
   j end
   
